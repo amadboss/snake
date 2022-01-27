f4308e22a7522da0e9253d77918b9903fc9dee58
Mohammad-Amad <79084290+amadboss@users.noreply.github.com>
2022-01-27 11:22:18 +0100

    Update Mons.py

--- Mons.py
+++ Mons.py
@@ -40,12 +40,12 @@ def show_score(color, font, size):
     score_rect.midtop = (taille_ecran_x / 10, 15)
     game_window.blit(score_surface, score_rect)
 
-def genere_mur():    
-    for i in range(nb_mur):
-        print("on entre",i,"fois")
+def genere_mur():
+    for i in range(0,nb_mur,1):
+        #print("on entre",i,"fois")
         #double tableau style [[0], [1260, 780], [1], [90, 450], [2], [1350, 960], [3], [1830, 30]]
         mur.extend(([i],[random.randrange(1,(taille_ecran_x // rect_taille)) * rect_taille,random.randrange(1,(taille_ecran_y // rect_taille)) * rect_taille]))
-        print(mur,"et i",i)
+        #print(mur,"et i",i)
 genere_mur()
 
 while True:
@@ -103,10 +103,14 @@ while True:
     #spawn de la pomme
     pygame.draw.rect(game_window,rouge, pygame.Rect(pomme[0], pomme[1], rect_taille // 2, rect_taille// 2))
     #pour chaque mure dans le tableau. aficher un carer gris corespondant a la valeur du tableau sous jaccent 
-    for j in range(0,nb_mur,2):
-        pygame.draw.rect(game_window,gris, pygame.Rect(mur[j+1][0], mur[j+1][1], rect_taille, rect_taille))
-        if tete[0] == mur[j+1][0] and tete[1] == mur[j+1][1]:
-            init_vars()
+    j = 0
+    for j in range(0,nb_mur*2,2):
+        if j <= nb_mur*2-1:
+            pygame.draw.rect(game_window,gris, pygame.Rect(mur[j+1][0], mur[j+1][1], rect_taille, rect_taille))
+            if tete[0] == mur[j+1][0] and tete[1] == mur[j+1][1]:
+                time.sleep(2)
+                init_vars()
+                genere_mur()
     for pos in corp:
         pygame.draw.rect(game_window, blanc, pygame.Rect(pos[0] , pos[1] ,rect_taille , rect_taille ))
         
@@ -115,7 +119,8 @@ while True:
 
     for block in corp[1:]:
         if tete[0] == block[0] and tete[1] == block[1]:
+            time.sleep(2)
             init_vars()
-
+            genere_mur()
     show_score(blanc, 'arial', 20)
     pygame.display.update()
